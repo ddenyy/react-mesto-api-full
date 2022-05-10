7bc06e680c095175b9cfcde19580a2f42b25b10c
Denis Osipov <ddennios@mail.ru>
2022-05-10 18:33:22 +0300

    fix & feat: исправил локальный запуск и добавил логирование запросов и ошибок

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -14,13 +14,26 @@ const {
   signInValidation,
 } = require('./middlewares/validations');
 const cors = require('./middlewares/corsHeaders');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
 const { PORT = 3000 } = process.env;
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // промежуток времени на запросы
+  max: 1000, // максимум 1000 запросов с одного ip
+});
+
 const app = express();
+
+// настраиваем заголовки
+app.use(helmet());
+// подключаем rate-limiter
+app.use(limiter);
 // подключение к бд
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.listen(PORT);
 // подключаем обработку приходящих данных
 app.use(express.json());
 // подключаем обработку куки, все куки лежат в req.cookie
@@ -33,6 +46,9 @@ app.get('/crash-test', () => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
+// логирование запросов
+app.use(requestLogger);
+
 app.post('/signin', signInValidation, login);
 app.post('/signup', signUpValidation, createUser);
 
@@ -46,5 +62,10 @@ app.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
 
+// логирование ошибок
+app.use(errorLogger);
+
 app.use(errors());
 app.use(errorHandler);
+
+app.listen(PORT);
