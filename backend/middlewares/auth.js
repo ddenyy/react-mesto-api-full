5bea06f05dea0733792607d9606d2111a41e9148
Denis Osipov <ddennios@mail.ru>
2022-05-14 21:17:08 +0300

    fix: исправил ошибку шифрования jwt

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,3 +1,4 @@
+const { JWT_SECRET = 'dev-key' } = process.env;
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
@@ -12,7 +13,7 @@ const auth = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     next(new UnauthorizedError('Нужна авторизация'));
   }
